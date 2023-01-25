1cb453f9c25be2969aedd61f22ea18da6f49aab0
AMkhitarants <avetis.mkhitaryants@gmail.com>
2023-01-25 21:35:23 +0300

    2. Добавление модели "Task"

---
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -0,0 +1,26 @@
+package ru.job4j.todo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+
+    private String description;
+    private LocalDateTime created = LocalDateTime.now();
+    private boolean done;
+}
